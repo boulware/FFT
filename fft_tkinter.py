ee7e7c1d119a009f2c0206dc745998956b719eca
xish <tyler.boulware@gmail.com>
2017-01-24 04:11:47 -0600

    Implement mic recording and its waveform/FFT display

--- fft_tkinter.py
+++ fft_tkinter.py
@@ -9,6 +9,14 @@ matplotlib.use('TkAgg')
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2TkAgg
 from matplotlib.figure import Figure
 
+import pyaudio
+import binascii
+import struct
+
+import sounddevice as sd
+
+print(sd.query_devices())
+
 import scipy.fftpack
 
 np.set_printoptions(threshold=np.inf)
@@ -25,6 +33,76 @@ WAVData = namedtuple(	'WAVData',
 root = tk.Tk()
 file = []
 fft_raw_data = []
+
+chunk = 1024
+FORMAT = pyaudio.paInt16
+CHANNELS = 1
+RATE = 44100
+
+def RecordMic():
+	p = pyaudio.PyAudio()
+	stream = p.open(format				= FORMAT,
+					channels			= CHANNELS,
+					rate				= RATE,
+					input				= True,
+					frames_per_buffer	= chunk)
+
+	mic_data = []
+	chunk_count = int(RATE / chunk * 1.0)
+
+	for i in range(0, chunk_count):
+		data = stream.read(chunk)
+		mic_data.append(data)
+
+	stream.stop_stream()
+	stream.close()
+	p.terminate()
+
+	mic_data = b''.join(mic_data) # flatten mic_data to a list of bytes
+
+	mic_ints = [] # to store mic_data encoded as 16-bit signed integers
+	print(str(len(mic_data[2*i:(2*i)+2])))
+	for i in range(0, len(mic_data) // 2):
+		mic_ints += struct.unpack('<h', mic_data[2*i:(2*i)+2])
+
+	waveFile = wave.open("_file.wav", 'wb')
+	waveFile.setnchannels(CHANNELS)
+	waveFile.setsampwidth(p.get_sample_size(FORMAT))
+	waveFile.setframerate(RATE)
+	waveFile.writeframes(mic_data)
+	waveFile.close()
+
+
+
+	fig = Figure(figsize=(5,5), dpi=100)
+	waveform_plot = fig.add_subplot(2, 1, 1)
+	fft_plot = fig.add_subplot(2, 1, 2)
+
+	n = len(mic_ints) # number of samples
+	k = np.arange(n)
+	t = k / RATE # Creates discrete array of time values for our sampling frequency
+	T = n / RATE # Sample length in seconds
+	frq = k / T
+	frq = frq[range(n//2)]
+	#frq = np.linspace(0.0, 1.0/(2.0*T), N/2)
+
+
+	global fft_raw_data
+	fft_raw_data = scipy.fftpack.fft(mic_ints)
+	fft_raw_data = fft_raw_data[range(n//2)]
+
+	print(str(len(t)))
+	print(str(len(mic_ints)))
+	waveform_plot.plot(t, mic_ints)
+	fft_plot.plot(frq, fft_raw_data)
+
+	canvas = FigureCanvasTkAgg(fig, master=root)
+	canvas.show()
+	canvas.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=1)
+
+	toolbar = NavigationToolbar2TkAgg(canvas, root)
+	toolbar.update()
+	canvas._tkcanvas.pack(side=tk.TOP, fill=tk.BOTH, expand=1)
 
 def OpenWAVFile():
 	file_name = filedialog.askopenfilename()
@@ -65,8 +143,8 @@ def OpenWAVFile():
 	canvas._tkcanvas.pack(side=tk.TOP, fill=tk.BOTH, expand=1)
 
 def ExportCSV():
-	csv_filename = filedialog.asksaveasfilename(filetypes=[("Comma-separated values", 'csv')])
-	with open(csv_filename + '.csv', 'wt') as csv_file:
+	csv_filename = filedialog.asksaveasfilename(filetypes=[("Comma-separated values", 'csv')], defaultextension='csv')
+	with open(csv_filename, 'wt') as csv_file:
 		global fft_raw_data
 		for e in fft_raw_data:
 			csv_file.write(str(e))
@@ -77,6 +155,7 @@ menubar = tk.Menu(root)
 filemenu = tk.Menu(menubar, tearoff=0)
 filemenu.add_command(label="Open...", command=OpenWAVFile, underline=0)
 filemenu.add_cascade(label="Export...", command=ExportCSV, underline=0)
+filemenu.add_cascade(label="Record", command=RecordMic, underline=0)
 menubar.add_cascade(label="File", menu=filemenu)
 root.config(menu=menubar)
 
